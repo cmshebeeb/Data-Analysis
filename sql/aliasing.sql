3dc1ab85b916bf7fd877ba56e3966e6d0b3ebcb2
Muhammed Shebeeb c <mshebeebc@gmail.com>
2024-03-06 17:48:27 +0530

    Update aliasing.sql

--- sql/aliasing.sql
+++ sql/aliasing.sql
@@ -1,2 +1,12 @@
 /*Aliasing:- temporarly changing the coloumn name or table name
 in script and not impact the output, it is used for more readablity */
+SELECT CONCAT(FirstName, ' ', LastName)  FullName
+FROM da_tutorial.employeedemographics;
+
+select avg(age) Avg_Age from employeedemographics;
+
+select demo.EmployeeID,sal.Salary from da_tutorial.employeedemographics as demo
+join da_tutorial.employeesalary 
+as sal on demo.EmployeeID=sal.EmployeeID;
+
+
